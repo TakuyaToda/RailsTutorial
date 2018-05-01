cdb8819b5776e26d2d17fe88d57b8cd08db574a2
takutoda <toda.takuya@prevent.co.jp>
2018-05-01 18:06:31 +0900

    add heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/guarded-crag-22964.git.herokuapp.com'
+  host = 'rails-tutorial-sample.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
