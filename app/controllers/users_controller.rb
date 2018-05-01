295091191a1ea84814b86bfca9fd35e9eb0267ba
takutoda <toda.takuya@prevent.co.jp>
2018-05-01 17:20:59 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,12 +20,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      UserMailer.account_activation(@user).deliver_now
-      # @user.send_activation_email
-      # flash[:info] = "Please check your email to activate your account."
-      # redirect_to root_url
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
+      # UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
